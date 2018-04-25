b010c4154786c76d6814179e8d44ce0fb79d94b0
shimeng <sm0150325@gmail.com>
2018-04-25 11:15:21 +0800

    complete the env

--- mvvm/01parcel/index.js
+++ mvvm/01parcel/index.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+import san from 'san';
+
+var MyApp = san.defineComponent({
+  template: '<p>Hello {{name}}!</p>',
+  initData: function() {
+    return {
+      name: 'World'
+    };
+  }
+});
+
+var myApp = new MyApp();
+myApp.attach(document.body);
